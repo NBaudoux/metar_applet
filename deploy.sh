9f742fff6463dc19b14c94535f4878a5ca980885
NBaudoux <baudouxn@gmail.com>
2026-04-17 18:48:27 +0200

    Add setting to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp -r applet.js ~/.local/share/cinnamon/applets/metar_applet@local/applet.js
 cp -r metadata.json ~/.local/share/cinnamon/applets/metar_applet@local/metadata.json
+cp -r settings-schema.json ~/.local/share/cinnamon/applets/metar_applet@local/settings-schema.json
